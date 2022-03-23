91b12dd412dbd700a3c039f1fc831a2c1295735f
Venusha <venusha125@gmail.com>
2022-03-23 08:29:01 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { BarLoader, Spinner, DoubleBubble,DoubleOrbit,HalfMalf,TripleMaze } from 'react-spinner-animated';
+import 'react-spinner-animated/dist/index.css'
+import { useState } from 'react';
+import Comp from './Comp';
 
 function App() {
+  const [component, setComponent] = useState("")
+  const [text, setText] = useState("Loading...")
+  const [isCentered, setIsCentered] = useState(true)
+  const [color, setColor] = useState("#557B83")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col bg-blue-50 w-full min-h-screen h-full items-center flex-wrap">
+      <h1 className='text-3xl m-5'>react-spinner-animated</h1>
+
+      <div className='flex flex-row w-full items-center justify-center mt-10'>
+        <label className="mx-3">Spinner/Loader</label>
+        <select className='bg-blue-300 w-1/5 rounded p-1'>
+          <option value="BarLoader">BarLoader</option>
+          <option value="Spinner">Spinner</option>
+          <option value="DoubleBubble">DoubleBubble</option>
+          <option value="DoubleOrbit">DoubleOrbit</option>
+          <option value="HalfMalf">HalfMalf</option>
+          <option value="TripleMaze">TripleMaze</option>
+        </select>
+
+        <label className="mx-3 ml-5">Text</label>
+        <input type="text" placeholder="Loading..."  className='w-1/5 rounded p-1 border-2 border-blue-300'/>
+      
+        
+        <label className="mx-3 ml-5">Center?</label>
+        <select className='bg-blue-300 w-1/5 rounded p-1'>
+          <option value="true">true</option>
+          <option value="false">false</option>
+         
+        </select>
+      </div>
+  
+     
+     <Comp component={<BarLoader text={text} bgColor={color} isCentered={isCentered}/>} />
     </div>
   );
 }
---
+++ src/Comp.js
@@ -0,0 +1,7 @@
+
+import { BarLoader, Spinner, DoubleBubble,DoubleOrbit,HalfMalf,TripleMaze } from 'react-spinner-animated';
+import 'react-spinner-animated/dist/index.css'
+export default function Comp({component, text, color, isCentered}) {
+
+    return (<>{component}</>)
+}
