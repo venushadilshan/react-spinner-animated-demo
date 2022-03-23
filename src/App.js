24239905bc9af14855ba0d710fad97d3ad4adf0b
Venusha <venusha125@gmail.com>
2022-03-23 10:58:19 +0530

    add color picker

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import { BarLoader, Spinner, DoubleBubble,DoubleOrbit,HalfMalf,TripleMaze } from 'react-spinner-animated';
+import { BarLoader, Spinner, DoubleBubble, DoubleOrbit, HalfMalf, TripleMaze } from 'react-spinner-animated';
 import 'react-spinner-animated/dist/index.css'
 import { useState } from 'react';
 import Comp from './Comp';
+import { SketchPicker } from 'react-color';
 
 function App() {
-  const [component, setComponent] = useState("")
-  const [text, setText] = useState("Loading...")
-  const [isCentered, setIsCentered] = useState(true)
-  const [color, setColor] = useState("#557B83")
-  return (
-    <div className="flex flex-col bg-blue-50 w-full min-h-screen h-full items-center flex-wrap">
-      <h1 className='text-3xl m-5'>react-spinner-animated</h1>
 
-      <div className='flex flex-row w-full items-center justify-center mt-10'>
+  const [text, setText] = useState("Loading...")
+  const [isCentered, setIsCentered] = useState(false)
+  const [color, setColor] = useState("#557B83")
+  const [component, setComponent] = useState("Spinner")
+  const [show, setShow] = useState(false)
+  
+
+  const selectComponent = (e) => {
+    
+
+   setComponent(e)
+   setShow(false)
+
+  }
+
+  const setTextVal = (e) => {
+    setText(e.target.value)
+    setShow(false)
+  }
+
+  const apply = () => {
+  setShow(true)
+  
+  }
+
+  const handleChangeComplete = (color) => {
+   
+  setColor(color.hex)
+  setShow(false)
+    
+  };
+
+  const showColorPicker=()=>{
+    
+  }
+
+  return (
+    <div className="flex flex-row bg-blue-50 w-full min-h-screen h-screen items-start justify-between ">
+   
+
+      <div className='flex flex-col w-1/3 items-center justify-center h-full'>
+      <h1 className='text-3xl m-5'>react-spinner-animated</h1>
         <label className="mx-3">Spinner/Loader</label>
-        <select className='bg-blue-300 w-1/5 rounded p-1'>
+        <select className='bg-blue-300 w-auto rounded p-1' onChange={(e) => { selectComponent(e.target.value) }}>
           <option value="BarLoader">BarLoader</option>
           <option value="Spinner">Spinner</option>
           <option value="DoubleBubble">DoubleBubble</option>
@@ -25,20 +60,31 @@ function App() {
           <option value="TripleMaze">TripleMaze</option>
         </select>
 
-        <label className="mx-3 ml-5">Text</label>
-        <input type="text" placeholder="Loading..."  className='w-1/5 rounded p-1 border-2 border-blue-300'/>
-      
-        
-        <label className="mx-3 ml-5">Center?</label>
-        <select className='bg-blue-300 w-1/5 rounded p-1'>
-          <option value="true">true</option>
-          <option value="false">false</option>
-         
-        </select>
+        <label className=" mt-5">Text</label>
+        <input type="text" placeholder="Loading..." className='w-auto rounded p-1 border-2 border-blue-300' onChange={(e) => { setTextVal(e) }} />
+
+
+        <label className="mx-3 mt-5">Color</label>
+       
+        <SketchPicker
+        color={ color}
+        onChangeComplete={handleChangeComplete }
+      />
+       <button className='bg-blue-400 p-1 m-3 rounded' onClick={apply}>Apply</button>
       </div>
-  
+
+      <div className='flex flex-col w-2/3 items-center justify-center w-full h-full'>
+        <div className='flex flex-col w-full'></div>
+     {(show) ?  <Comp component={component} text={text} color={color} isCentered={isCentered}/> : null}
+     <p className='w-3/5 rounded-lg border-2 border-blue-400 p-3 text-sm'>{`<${component} text={${text}} bgColor={${color}} />`}</p>
+      
+      </div>
      
-     <Comp component={<BarLoader text={text} bgColor={color} isCentered={isCentered}/>} />
+
+     
+
+
+
     </div>
   );
 }
--- src/Comp.js
+++ src/Comp.js
@@ -2,6 +2,15 @@
 import { BarLoader, Spinner, DoubleBubble,DoubleOrbit,HalfMalf,TripleMaze } from 'react-spinner-animated';
 import 'react-spinner-animated/dist/index.css'
 export default function Comp({component, text, color, isCentered}) {
+    
+  
+    
+    if (component === "BarLoader") return(<BarLoader text={text} bgColor={color} center={isCentered} width="150px" />)
+    else if (component === "Spinner") return(<Spinner text={text} bgColor={color} center={isCentered} width="150px" />)
+    else if (component === "DoubleOrbit") return(<DoubleOrbit text={text} bgColor={color} center={isCentered} width="150px" />)
+    else if (component === "DoubleBubble") return(<DoubleBubble text={text} bgColor={color} center={isCentered} width="150px" />)
+    else if (component === "HalfMalf") return(<HalfMalf text={text} bgColor={color} center={isCentered} width="150px" />)
+    else if (component === "TripleMaze") return(<TripleMaze text={text} bgColor={color} center={isCentered} width="150px" />)
 
-    return (<>{component}</>)
+   
 }
