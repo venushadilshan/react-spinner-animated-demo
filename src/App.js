c6b396b6838df7c25de1e034ef13d3a70d17a7a0
Venusha <venusha125@gmail.com>
2022-03-24 06:25:52 +0530

    add height, width fix tyext string quotes

--- src/App.js
+++ src/App.js
@@ -4,52 +4,63 @@ import { BarLoader, Spinner, DoubleBubble, DoubleOrbit, HalfMalf, TripleMaze } f
 import 'react-spinner-animated/dist/index.css'
 import { useState } from 'react';
 import Comp from './Comp';
-import { SketchPicker } from 'react-color';
+import { HuePicker } from 'react-color';
 
 function App() {
 
   const [text, setText] = useState("Loading...")
   const [isCentered, setIsCentered] = useState(false)
   const [color, setColor] = useState("#557B83")
-  const [component, setComponent] = useState("Spinner")
+  const [component, setComponent] = useState("BarLoader")
   const [show, setShow] = useState(false)
-  
+  const [height, setHeight] = useState("150")
+  const [width, setWidth] = useState("150")
+
+
 
   const selectComponent = (e) => {
-    
 
-   setComponent(e)
-   setShow(false)
+
+    setComponent(e)
+    setShow(false)
 
   }
 
   const setTextVal = (e) => {
-    setText(e.target.value)
+    setText(e)
     setShow(false)
   }
 
   const apply = () => {
-  setShow(true)
-  
+    setShow(true)
+
   }
 
   const handleChangeComplete = (color) => {
-   
-  setColor(color.hex)
-  setShow(false)
-    
+
+    setColor(color.hex)
+    setShow(false)
+
   };
 
-  const showColorPicker=()=>{
-    
+  const setComponentHeight = (e) => {
+    setHeight(e)
+    setShow(false)
+  }
+
+  const setComponentWidth = (e) => {
+    setWidth(e)
+    setShow(false)
+   
+
   }
 
   return (
     <div className="flex flex-row bg-blue-50 w-full min-h-screen h-screen items-start justify-between ">
-   
+
 
       <div className='flex flex-col w-1/3 items-center justify-center h-full'>
-      <h1 className='text-3xl m-5'>react-spinner-animated</h1>
+        <h1 className='text-3xl m-5'>react-spinner-animated</h1>
         <label className="mx-3">Spinner/Loader</label>
         <select className='bg-blue-300 w-auto rounded p-1' onChange={(e) => { selectComponent(e.target.value) }}>
           <option value="BarLoader">BarLoader</option>
@@ -59,29 +70,41 @@ function App() {
           <option value="HalfMalf">HalfMalf</option>
           <option value="TripleMaze">TripleMaze</option>
         </select>
+        <div className='flex flex-row w-4/5 flex-wrap items-center justify-center mt-5'>
 
-        <label className=" mt-5">Text</label>
-        <input type="text" placeholder="Loading..." className='w-auto rounded p-1 border-2 border-blue-300' onChange={(e) => { setTextVal(e) }} />
+          <input type="text" placeholder="Custom Text" className='w-full rounded border-2 border-blue-300' onChange={(e) => { setTextVal(e.target.value) }} />
+
+          <div className='flex flex-row w-full mt-5'>
+            <input type="number" placeholder="Width (px)" className='mr-1 w-full rounded border-2 border-blue-300' onChange={(e) => { setComponentWidth(e.target.value) }} />
+
+
+            <input type="number" placeholder="Height (px)" className='ml-1 w-full rounded  border-2 border-blue-300' onChange={(e) => { setComponentHeight(e.target.value) }} />
+          </div>
+
+        </div>
+
+
 
 
         <label className="mx-3 mt-5">Color</label>
-       
-        <SketchPicker
-        color={ color}
-        onChangeComplete={handleChangeComplete }
-      />
-       <button className='bg-blue-400 p-1 m-3 rounded' onClick={apply}>Apply</button>
+
+        <HuePicker
+          color={color}
+          onChangeComplete={handleChangeComplete}
+        />
+        <button className='bg-blue-400 p-1 m-3 rounded w-1/3' onClick={apply}>Apply</button>
       </div>
 
       <div className='flex flex-col w-2/3 items-center justify-center w-full h-full'>
         <div className='flex flex-col w-full'></div>
-     {(show) ?  <Comp component={component} text={text} color={color} isCentered={isCentered}/> : null}
-     <p className='w-3/5 rounded-lg border-2 border-blue-400 p-3 text-sm'>{`<${component} text={${text}} bgColor={${color}} />`}</p>
-      
-      </div>
-     
+        {(show) ? <Comp component={component} text={text} color={color} isCentered={isCentered} height={height} width={width} /> : null}
+        <p className='w-3/5 rounded-lg border-2 border-blue-400 p-3 text-sm'>{`<${component} text={"${text}"} bgColor={"${color}"} 
+        width={"${width}px"} height={"${height}px"} />`}</p>
 
-     
+      </div>
+
+
+
 
 
 
--- src/Comp.js
+++ src/Comp.js
@@ -1,16 +1,16 @@
 
 import { BarLoader, Spinner, DoubleBubble,DoubleOrbit,HalfMalf,TripleMaze } from 'react-spinner-animated';
 import 'react-spinner-animated/dist/index.css'
-export default function Comp({component, text, color, isCentered}) {
+export default function Comp({component, text, color, isCentered, height, width}) {
     
   
-    
-    if (component === "BarLoader") return(<BarLoader text={text} bgColor={color} center={isCentered} width="150px" />)
-    else if (component === "Spinner") return(<Spinner text={text} bgColor={color} center={isCentered} width="150px" />)
-    else if (component === "DoubleOrbit") return(<DoubleOrbit text={text} bgColor={color} center={isCentered} width="150px" />)
-    else if (component === "DoubleBubble") return(<DoubleBubble text={text} bgColor={color} center={isCentered} width="150px" />)
-    else if (component === "HalfMalf") return(<HalfMalf text={text} bgColor={color} center={isCentered} width="150px" />)
-    else if (component === "TripleMaze") return(<TripleMaze text={text} bgColor={color} center={isCentered} width="150px" />)
+    console.log(width)
+    if (component === "BarLoader") return(<BarLoader text={text} bgColor={color} center={isCentered} width={width+"px"} height={height+"px"}/>)
+    else if (component === "Spinner") return(<Spinner text={text} bgColor={color} center={isCentered} width={width+"px"} height={height+"px"}/>)
+    else if (component === "DoubleOrbit") return(<DoubleOrbit text={text} bgColor={color} center={isCentered} width={width+"px"} height={height+"px"}/>)
+    else if (component === "DoubleBubble") return(<DoubleBubble text={text} bgColor={color} center={isCentered} width={width+"px"} height={height+"px"}/>)
+    else if (component === "HalfMalf") return(<HalfMalf text={text} bgColor={color} center={isCentered} width={width+"px"} height={height+"px"}/>)
+    else if (component === "TripleMaze") return(<TripleMaze text={text} bgColor={color} center={isCentered} width={width+"px"} height={height+"px"}/>)
 
    
 }
